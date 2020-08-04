65ef8acc51bc425d9dc60ea808cd81872b3e564d
KevinGines1 <kagines1@up.edu.ph>
2020-08-04 23:20:15 +0800

    test2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/', express.static('./documentation'))
 app.use((req,res,next)=>{
 	// res.header("Access-Control-Allow-Origin", "http://localhost:3000")
 	// res.header("Access-Control-Allow-Origin", "*")
-	// res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
+	res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
 	// res.header("Access-Control-Allow-Credentials", true);
 	// res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
 
