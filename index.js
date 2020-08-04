eb13156bb84f31017ed2ac990976177c9b7b5c6c
KevinGines1 <kagines1@up.edu.ph>
2020-08-04 19:14:17 +0800

    add cors

--- index.js
+++ index.js
@@ -20,27 +20,27 @@ app.use('/', express.static('./documentation'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
-// app.use(cors({
-//     origin:[
-//         'http://localhost:3000'
-//     ],
-//     credentials:true
-// }))
+app.use(cors({
+    origin:[
+        'http://localhost:3000'
+    ],
+    credentials:true
+}))
 
 // allow different origins to access the server
-app.use((req,res,next)=>{
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Headers", "*")
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+// app.use((req,res,next)=>{
+// 	res.header("Access-Control-Allow-Origin", "*")
+// 	res.header("Access-Control-Allow-Headers", "*")
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
 	
-	if(req.method === "OPTIONS"){
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-		return res.status(200).json({})
-	}
+// 	if(req.method === "OPTIONS"){
+// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+// 		return res.status(200).json({})
+// 	}
 
-	next();
-})
+// 	next();
+// })
 
 app.use(cookieParser())
 
