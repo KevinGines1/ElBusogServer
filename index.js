2309b04ac8ebf91b04727005366ec46517088645
KevinGines1 <kagines1@up.edu.ph>
2020-08-04 22:38:43 +0800

    re-add creds

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req,res,next)=>{
 		res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
 		// res.header("Access-Control-Allow-Origin", "*")
 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-		// res.header("Access-Control-Allow-Credentials", true);
+		res.header("Access-Control-Allow-Credentials", true);
 		// return res.status(200).json({})
 	}
 
