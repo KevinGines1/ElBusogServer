26a45f3b5a4bb16ea06049b1cc5c8cfe79f02c62
KevinGines1 <kagines1@up.edu.ph>
2020-08-06 14:36:00 +0800

    add cors

--- index.js
+++ index.js
@@ -16,32 +16,33 @@ const app = express()
 // middleware
 // app.use(express.static('documentation'))
 app.use('/', express.static('./documentation'))
+app.use(cors())
 
 // allow different origins to access the server
-app.use((req,res,next)=>{
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-	// res.header("Access-Control-Allow-Credentials", true);
-	// res.header("Access-Control-Allow-Origin", "*")
-	// res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
-	// res.header("Access-Control-Allow-Credentials", true);
-	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS')
+// app.use((req,res,next)=>{
+// 	res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+// 	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+// 	// res.header("Access-Control-Allow-Credentials", true);
+// 	// res.header("Access-Control-Allow-Origin", "*")
+// 	// res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
+// 	// res.header("Access-Control-Allow-Credentials", true);
+// 	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS')
 
 
 
-	// if(req.method === "OPTIONS"){
-	// 	res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-	// 	// res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
-	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-	// 	// res.header("Access-Control-Allow-Headers", "*")
-	// 	// res.header("Access-Control-Allow-Origin", "*")
-	// 	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-	// 	res.header("Access-Control-Allow-Credentials", true);
-	// 	// return res.status(200).json({})
-	// }
+// 	// if(req.method === "OPTIONS"){
+// 	// 	res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+// 	// 	// res.header("Access-Control-Allow-Origin", "https://www.test-cors.org")
+// 	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+// 	// 	// res.header("Access-Control-Allow-Headers", "*")
+// 	// 	// res.header("Access-Control-Allow-Origin", "*")
+// 	// 	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+// 	// 	res.header("Access-Control-Allow-Credentials", true);
+// 	// 	// return res.status(200).json({})
+// 	// }
 
-	next();
-})
+// 	next();
+// })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
