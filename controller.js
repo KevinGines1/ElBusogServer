d7c408e7b2e3e16ce2bf2853c4a6cd11e7fadbf4
KevinGines1 <kagines1@up.edu.ph>
2020-08-11 17:15:57 +0800

    change parameters of delete food place photo to an object

--- controller.js
+++ controller.js
@@ -330,8 +330,10 @@ module.exports.deleteAccountCustomer = (req,res) =>{	//Delete commentsAndRatings
 
 module.exports.deleteFoodPlacePhoto = (req,res) =>{ // one photo at a time
 	//input
-	const foodPlaceID = Number (req.params.foodPlaceID)
-	const foodPlacePhoto = req.params.foodPlacePhoto
+	// const foodPlaceID = Number (req.params.foodPlaceID)
+	const foodPlaceID = Number (req.params.foodPhotoInfo.foodPlaceID)
+	// const foodPlacePhoto = req.params.foodPlacePhoto
+	const foodPlacePhoto = req.params.foodPhotoInfo.foodPlacePhoto
 
 	//create query
 	const deletePictureQuery = `DELETE FROM FOOD_PLACE_PICTURES WHERE (Food_place_id=${foodPlaceID} AND Picture="${foodPlacePhoto}")`
